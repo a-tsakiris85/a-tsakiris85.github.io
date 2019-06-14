18cacd0ff7dd9bdc412444faade25b0725d61cd2
a-tsakiris85 <art85@cornell.edu>
2019-06-13 22:55:40 -0700

    initial version of typing 'i am' title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Typist from "react-typist";
+import TypistLoop from "react-typist-loop";
+
+const PHRASES = [
+  "Andrew Tsakiris",
+  "a cool guy",
+  "your worst nightmare",
+  "your mom"
+];
+
+const cursor = {
+  show: false,
+  blink: true,
+  element: "|",
+  hideWhenDone: true,
+  hideWhenDoneDelay: 0
+};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      typing: true,
+      textIndex: 0
+    };
+  }
+  done = () => {
+    this.setState(
+      { typing: false, textIndex: (this.state.textIndex + 1) % 4 },
+      () => this.setState({ typing: true })
+    );
+  };
+
+  render() {
+    let text = PHRASES[this.state.textIndex];
+    return (
+      <div>
+        <div style={{ float: "left" }}>
+          <h1 style={{ margin: "0 10px 0 0" }}>I am</h1>
+        </div>
+        {this.state.typing ? (
+          <Typist
+            style={{ float: "left" }}
+            onTypingDone={this.done}
+            cursor={{ show: false }}
+          >
+            <h1>{text} </h1>
+            <Typist.Delay ms={1000} />
+            <Typist.Backspace count={text.length - 1} />
+          </Typist>
+        ) : (
+          <span>{}</span>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
