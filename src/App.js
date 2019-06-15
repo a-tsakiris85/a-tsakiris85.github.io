ec5d0cbe22b3cca1624ccd1888466d27fa8149e1
a-tsakiris85 <art85@cornell.edu>
2019-06-14 23:26:50 -0700

    introduce color scheme. Center title. Navbar

--- src/App.js
+++ src/App.js
@@ -2,58 +2,27 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import Typist from "react-typist";
-import TypistLoop from "react-typist-loop";
+import TypingTitle from "./TypingTitle";
 
-const PHRASES = [
-  "Andrew Tsakiris",
-  "a cool guy",
-  "your worst nightmare",
-  "your mom"
-];
+import {
+  Navbar,
+  NavDropdown,
+  Nav,
+  Form,
+  Button,
+  FormControl
+} from "react-bootstrap";
 
-const cursor = {
-  show: false,
-  blink: true,
-  element: "|",
-  hideWhenDone: true,
-  hideWhenDoneDelay: 0
-};
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      typing: true,
-      textIndex: 0
-    };
-  }
-  done = () => {
-    this.setState(
-      { typing: false, textIndex: (this.state.textIndex + 1) % 4 },
-      () => this.setState({ typing: true })
-    );
-  };
-
   render() {
-    let text = PHRASES[this.state.textIndex];
     return (
-      <div>
-        <div style={{ float: "left" }}>
-          <h1 style={{ margin: "0 10px 0 0" }}>I am</h1>
+      <div id="appcontainer">
+        <Navbar bg="light" expand="lg" className="NavBar">
+          <Navbar.Brand id="title">ANDREW TSAKIRIS</Navbar.Brand>
+        </Navbar>
+        <div id="typingtitle">
+          <TypingTitle />
         </div>
-        {this.state.typing ? (
-          <Typist
-            style={{ float: "left" }}
-            onTypingDone={this.done}
-            cursor={{ show: false }}
-          >
-            <h1>{text} </h1>
-            <Typist.Delay ms={1000} />
-            <Typist.Backspace count={text.length - 1} />
-          </Typist>
-        ) : (
-          <span>{}</span>
-        )}
       </div>
     );
   }
---
+++ src/TypingTitle.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Typist from "react-typist";
+import TypistLoop from "react-typist-loop";
+import "./TypingTitle.css";
+
+const PHRASES = [
+  "Andrew Tsakiris",
+  "a Proud Cornellian",
+  "a Computer Scientist",
+  "a Problem Solver"
+];
+
+const cursor = {
+  show: false,
+  blink: true,
+  element: "|",
+  hideWhenDone: true,
+  hideWhenDoneDelay: 0
+};
+
+export default class TypingTitle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      typing: true,
+      textIndex: 0
+    };
+  }
+  done = () => {
+    this.setState(
+      { typing: false, textIndex: (this.state.textIndex + 1) % 4 },
+      () => this.setState({ typing: true })
+    );
+  };
+  render() {
+    let text = PHRASES[this.state.textIndex];
+    return (
+      <div>
+        <div id="iamdiv" style={{ float: "left" }}>
+          <h1 id="iam" style={{ margin: "0 10px 0 0" }}>
+            I AM
+          </h1>
+        </div>
+        {this.state.typing ? (
+          <Typist
+            style={{ float: "left" }}
+            onTypingDone={this.done}
+            cursor={{ show: false }}
+          >
+            <h1 id="typingtext">{text} </h1>
+            <Typist.Delay ms={1000} />
+            <Typist.Backspace count={text.length - 1} />
+          </Typist>
+        ) : (
+          <span>{}</span>
+        )}{" "}
+      </div>
+    );
+  }
+}
