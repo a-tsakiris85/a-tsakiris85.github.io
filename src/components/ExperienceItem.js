1ba90d15e842ff561de35fe2c673ffe2db71c7db
a-tsakiris85 <art85@cornell.edu>
2019-06-23 22:44:18 -0700

    setup for experiences

---
+++ src/components/ExperienceItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./ExperienceItem.css";
+
+export default class ExperienceItem extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is an experience </p>
+      </div>
+    );
+  }
+}
+
+/*
+  An experience should have a photo.
+  Name of position.
+  Organization.
+  Date (start and end)
+  Description
+  Relevant links?
+*/
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,11 +1,36 @@
 import React from "react";
+import ExperienceItem from "../components/ExperienceItem";
 
 export default class Experience extends React.Component {
   render() {
     return (
       <div className="Page">
-        <p> Experience Page </p>
+        <ExperienceItem />
+        <ExperienceItem />
       </div>
     );
   }
 }
+
+/*
+  My experiences:
+  Ridgeline Internship
+  Temple Research
+  TA
+  College Academy Instructor
+  -----
+  Involvements section??
+  Tour Guide
+  TA (again?)
+  TT Philanthropy Chair
+  Outdoor Odyssey Guide
+  ------------
+  Projects
+  Temple Research
+  Arcade Baseball
+  Critter World
+  This Website
+  Trails App?
+  -----------
+  Course work section? could be good....
+*/
