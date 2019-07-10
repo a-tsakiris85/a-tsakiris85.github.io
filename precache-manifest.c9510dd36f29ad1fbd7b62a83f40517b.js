7d3e17fa6dddc72a9baa83014170d45386830943
a-tsakiris85 <art85@cornell.edu>
2019-07-09 22:28:50 -0700

    deploy scripts

---
+++ precache-manifest.c9510dd36f29ad1fbd7b62a83f40517b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5519f5e48abac8923684de66b84141f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "285f9f2bb1adc22ff2ee",
+    "url": "/static/css/main.faf30c4e.chunk.css"
+  },
+  {
+    "revision": "e8c15ba50308052e37df",
+    "url": "/static/js/2.a2843810.chunk.js"
+  },
+  {
+    "revision": "285f9f2bb1adc22ff2ee",
+    "url": "/static/js/main.301c934b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ce198c29b44723dbf40e7f7a75a89fc7",
+    "url": "/static/media/acton.ce198c29.png"
+  },
+  {
+    "revision": "f4b63b896bf062e13a847a8108fa2711",
+    "url": "/static/media/college_academy.f4b63b89.jpg"
+  },
+  {
+    "revision": "44e74408dd48902a8097247d362c16e8",
+    "url": "/static/media/conant.44e74408.gif"
+  },
+  {
+    "revision": "b44c6f5bc40eeaf285f95244ce34e8b9",
+    "url": "/static/media/cornell.b44c6f5b.png"
+  },
+  {
+    "revision": "b3f99e0fbae6d86e8946b7267a8a2393",
+    "url": "/static/media/game.b3f99e0f.png"
+  },
+  {
+    "revision": "d181cbfb295d480e11de3c0e89b68fcf",
+    "url": "/static/media/learning.d181cbfb.png"
+  },
+  {
+    "revision": "14fbd1b57e3f429c489db79530627a33",
+    "url": "/static/media/profile.14fbd1b5.gif"
+  },
+  {
+    "revision": "53a86ef19f955dbd95bcb7423b6f4617",
+    "url": "/static/media/programming.53a86ef1.png"
+  },
+  {
+    "revision": "e8f4eb42adf58d40c7c2218c369a7f4a",
+    "url": "/static/media/speaking.e8f4eb42.png"
+  }
+]);
