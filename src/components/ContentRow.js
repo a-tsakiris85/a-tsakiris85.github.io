079cb81efe8123362d872665fe149c20ddb9cfdc
a-tsakiris85 <art85@cornell.edu>
2019-07-16 21:11:34 -0700

    centered stuff

--- src/components/ContentRow.js
+++ src/components/ContentRow.js
@@ -9,27 +9,29 @@ export default class ContentRow extends React.Component {
 
   render() {
     return (
-      <div id="ContentRow">
+      <>
         <h1 id="ContentTitle">{this.props.overallTitle}</h1>
-        <ContentItem
-          img={this.props.img1}
-          title={this.props.title1}
-          desc={this.props.desc1}
-          col={"1"}
-        />
-        <ContentItem
-          img={this.props.img2}
-          title={this.props.title2}
-          desc={this.props.desc2}
-          col={"2"}
-        />
-        <ContentItem
-          img={this.props.img3}
-          title={this.props.title3}
-          desc={this.props.desc3}
-          col={"3"}
-        />
-      </div>
+        <div id="ContentRow">
+          <ContentItem
+            img={this.props.img1}
+            title={this.props.title1}
+            desc={this.props.desc1}
+            col={"1"}
+          />
+          <ContentItem
+            img={this.props.img2}
+            title={this.props.title2}
+            desc={this.props.desc2}
+            col={"2"}
+          />
+          <ContentItem
+            img={this.props.img3}
+            title={this.props.title3}
+            desc={this.props.desc3}
+            col={"3"}
+          />
+        </div>
+      </>
     );
   }
 }
